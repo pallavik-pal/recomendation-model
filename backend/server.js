106036eb74d7ef23677bcf0574b3a4a92aebcc40
pallavi <pallavik15092004@gmail.com>
2025-04-27 19:23:55 +0530

    apiurl

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const API=require(".")
+
 
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
